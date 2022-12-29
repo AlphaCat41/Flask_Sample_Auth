ef7c746d93ccf2a66a495940ef974dd779e89db9
AlphaCat41 <pakdee.hn@outlook.com>
2022-12-29 10:28:39 +0700

    sql file

---
+++ User.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE myDB;
+USE myDB;
+CREATE TABLE User(
+	username varchar(255),
+    password varchar(255)
+    
+);
+
+INSERT INTO User (username, password)
+VALUES ('pak', '123456');
