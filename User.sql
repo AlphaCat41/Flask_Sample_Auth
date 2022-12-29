ad0a6624d6a544f22696216a918d82bc45d6fc81
Pakdee H <100037254+AlphaCat41@users.noreply.github.com>
2022-12-29 11:02:15 +0700

    Update User.sql

--- User.sql
+++ User.sql
@@ -2,8 +2,7 @@ CREATE DATABASE myDB;
 USE myDB;
 CREATE TABLE User(
 	username varchar(255),
-    password varchar(255)
-    
+    	password varchar(255)
 );
 
 INSERT INTO User (username, password)
